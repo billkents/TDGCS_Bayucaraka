a7db86540d2db6e94d4911b35c3f3cb5dc51a97c
Bill Kent <billkent289@gmail.com>
2025-02-02 22:12:00 +0700

    Update movingStepper+Servo.cpp

--- movingStepper+Servo.cpp
+++ movingStepper+Servo.cpp
@@ -1,1 +1,62 @@
+#include <Arduino.h>
+#include <Servo.h>
 
+#define DIR_PIN 3      // Pin untuk mengontrol arah
+#define STEP_PIN 4     // Pin untuk langkah
+#define ENABLE_PIN 2   // Pin untuk mengaktifkan driver (opsional)
+
+Servo myServo;
+
+float steps;       // OUTPUT
+int step_delay = 500;  // Delay antar langkah dalam mikrodetik
+
+void moveStepper(int stepCount) {
+  for (int i = 0; i < stepCount; i++) {
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(step_delay);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(step_delay);
+  }
+}
+
+void setup() {
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+  myServo.attach(7);
+  myServo.write(0);
+  
+  digitalWrite(ENABLE_PIN, LOW);  // Aktifkan driver
+  
+  Serial.begin(9600);  // Mulai komunikasi Serial
+  Serial.println("Ingin digerakkan berapa derajat:");
+}
+
+void loop() {
+  gerakServo();
+  //gerakStepper();
+}
+
+void gerakStepper() {
+  if (Serial.available() > 0) {
+    int newSteps = Serial.parseInt(); // Baca input sebagai integer
+    steps = abs(newSteps/1.8);
+    if (newSteps != 0) {
+      int ikiSteps = newSteps;
+      Serial.print("Derajat stepper: ");
+      Serial.println(ikiSteps);
+      digitalWrite(DIR_PIN, newSteps > 0 ? HIGH : LOW);
+      moveStepper(steps);
+    }
+  }
+}
+
+void gerakServo() {
+  int inputAngle = Serial.parseInt(); // Baca input sebagai integer
+  int servoAngle = abs(inputAngle);
+  if (inputAngle >= 0) {
+    Serial.print("Derajat servo: ");
+    Serial.println(servoAngle);
+    myServo.write(servoAngle);
+  }
+}
